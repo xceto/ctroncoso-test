0a907453edd7cb8f0ae262fc9ff6c006e9e03d74
ctroncoso <claudio.troncoso91@gmail.com>
2023-01-16 00:10:41 -0300

    remove folder build and coverage

--- build/src/config/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.jwtKeys = void 0;
-exports.jwtKeys = {
-  secret: process.env.JWT_SECRET,
-  exp: process.env.JWT_EXP,
-};
-//# sourceMappingURL=config.js.map
